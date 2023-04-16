82e8ac56e8eed688fe31f96dea6dd11a86b464d1
shreypachauri3 <78973003+shreypachauri3@users.noreply.github.com>
2023-04-16 17:52:23 +0530

    Updated by removing commented code

--- agent.py
+++ agent.py
@@ -65,9 +65,7 @@ def train():
     game = Game()
     agent = Agent()
     record = 0
-    # total_scores = 0
-    # plot_scores = []
-    # plot_mean_scores =[]
+   
     while True:
         #get current state
         state_old = game.get_state()
@@ -97,11 +95,7 @@ def train():
             print("Game ", agent.n_games,' Scores ', score , ' Records: ',record, ' Epsilon: ' , agent.epsilon)
                     
             agent.k = []
-            # plot_scores.append(score)
-            # total_scores += score
-            # mean_score = total_scores/agent.n_games
-            # plot_mean_scores.append(mean_score)
-            # plot(plot_scores,plot_mean_scores)
+           
               
 
 if __name__ == '__main__':
